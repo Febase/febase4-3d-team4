c5f120bef9a7fbdcfa08b34894502b31e0219ba0
wooooooood <13pft13@gmail.com>
2023-02-25 18:38:40 +0900

    feat. set space background color

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import { css } from "@emotion/react";
 import { Canvas } from "@react-three/fiber";
 import { Box } from "./components/Box";
+import * as THREE from "three";
 import G0Universe from "./components/G0/";
 
 export function ThreeCanvas() {
   return (
     <div css={canvasCss}>
-      <Canvas>
+      <Canvas
+        onCreated={({ gl }) => {
+          gl.toneMapping = THREE.Uncharted2ToneMapping
+          gl.setClearColor(new THREE.Color('#020207'))
+      }}>
         <ambientLight />
         <pointLight position={[10, 10, 10]} />
         <Box position={[-1.2, 0, 0]} />
